ae2cfa075f9bda0bb2f929ba2a1c430a792436c2
Ryan Townsend <ryan@thinkrefresh.com>
2009-12-01 11:50:15 +0000

    Set min date automatically

--- lib/sieve/date_range.rb
+++ lib/sieve/date_range.rb
@@ -10,12 +10,12 @@ module Sieve
     
     def set_date(type, attribute)
       self.send("#{type}=".to_sym, DateTime.civil(
-        (attribute["#{type}(1i)"] || 0).to_i,
-        (attribute["#{type}(2i)"] || 0).to_i,
-        (attribute["#{type}(3i)"] || 0).to_i,
+        [(attribute["#{type}(1i)"] || 0).to_i, Date.today.year - 5].max,
+        [(attribute["#{type}(2i)"] || 0).to_i, 1].max,
+        [(attribute["#{type}(3i)"] || 0).to_i, 1].max,
         (attribute["#{type}(4i)"] || 0).to_i,
         (attribute["#{type}(5i)"] || 0).to_i
-      )) if attribute.respond_to?(:select) && attribute.select { |k,v| k =~ /^[from|to]/ }.length > 0
+      )) if attribute.respond_to?(:select) && attribute.select { |k,v| k =~ /^[from|to]/ && v.present? }.length > 0
     rescue ArgumentError
       raise Sieve::InvalidDateError, "The '#{type}' date is invalid:\n#{attribute.select { |k,v| k =~ /^[from|to]/ }.map { |k,v| "#{k} => #{v}" }.join("\n")}"
     end
