29e8242a4c81bc2928f9d9fd700c1ad4f2966721
Ryan Townsend <ryan@thinkrefresh.com>
2009-12-02 09:43:05 +0000

    Max out date ranges with todays date

--- lib/sieve/date_range.rb
+++ lib/sieve/date_range.rb
@@ -9,15 +9,15 @@ module Sieve
     end
     
     def set_date(type, attribute)
-      self.send("#{type}=".to_sym, DateTime.civil(
+      self.send("#{type}=".to_sym, [DateTime.civil(
         [(attribute["#{type}(1i)"] || 0).to_i, Date.today.year - 5].max,
         [(attribute["#{type}(2i)"] || 0).to_i, 1].max,
         [(attribute["#{type}(3i)"] || 0).to_i, 1].max,
         (attribute["#{type}(4i)"] || 0).to_i,
         (attribute["#{type}(5i)"] || 0).to_i
-      )) if attribute.respond_to?(:select) && attribute.select { |k,v| k =~ /^[from|to]/ && v.present? }.length > 0
+      ), DateTime.now].min) if attribute.respond_to?(:select) && attribute.select { |k,v| k =~ /^#{type}/ && v.present? }.length > 0
     rescue ArgumentError
-      raise Sieve::InvalidDateError, "The '#{type}' date is invalid:\n#{attribute.select { |k,v| k =~ /^[from|to]/ }.map { |k,v| "#{k} => #{v}" }.join("\n")}"
+      raise Sieve::InvalidDateError, "The '#{type}' date is invalid:\n#{attribute.select { |k,v| k =~ /^#{type}/ }.map { |k,v| "#{k} => #{v}" }.join("\n")}"
     end
   end
   
